0656727f7cb7541325563fb7f747bb342b181f68
mondalratul2006-ai <mondalratul2006@gmail.com>
2026-09-18 13:19:15 +0530

    Create BubbleSorting.java

---
+++ DSA WITH JAVA/Sorting Algorithms/BubbleSorting.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class bubbleSorting {
+
+    static void printArray(int[] arr, int n) {
+        System.out.println("\nArray:- ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the no. of elements = ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.err.print("Enter the elements = ");
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = sc.nextInt();
+        printArray(arr, n);
+        bubbleSort(arr);
+        printArray(arr, n);
+        sc.close();
+    }
+}
