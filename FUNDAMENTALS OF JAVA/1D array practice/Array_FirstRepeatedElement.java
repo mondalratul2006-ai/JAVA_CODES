e9f33686b980a286500307b4a5536dcbc67bbc65
mondalratul2006-ai <mondalratul2006@gmail.com>
2026-09-26 20:30:33 +0530

    Create Array_FirstRepeatedElement.java

---
+++ FUNDAMENTALS OF JAVA/1D array practice/Array_FirstRepeatedElement.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Array_FirstRepeatedElement {
+    static int firstRepeated(int a[]) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[j] == a[i]) {
+                    return a[i];
+                }
+            }
+        }
+        return -1;
+    }
+
+    static void printArray(int a[]) {
+        System.out.print("\nArray = ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Number Of Element = ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.print("Enter the Element(s)= ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        printArray(arr);
+        System.out.println("Repeated 1st Element = " + firstRepeated(arr));
+        sc.close();
+    }
+}
